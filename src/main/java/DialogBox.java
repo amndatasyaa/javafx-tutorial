e39f1f64a0d172e81e9d4fc29baa52128eae30ff
amndatasyaa <amandatasyadedi@gmail.com>
2026-09-01 16:42:47 +0800

    Enable interactive Duke conversations
    Handle text field and button events and show Duke's generated responses.
    
    Differentiate both speakers and keep the newest messages visible.

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,4 +1,7 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -28,5 +31,39 @@ public class DialogBox extends HBox {
         setAlignment(Pos.TOP_RIGHT);
 
         getChildren().addAll(text, displayPicture);
+    }
+
+    /**
+     * Flips the dialog box so that the display picture is on the left.
+     */
+    private void flip() {
+        setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> children = FXCollections.observableArrayList(getChildren());
+        FXCollections.reverse(children);
+        getChildren().setAll(children);
+    }
+
+    /**
+     * Creates a dialog box for a message sent by the user.
+     *
+     * @param message Text sent by the user.
+     * @param image User's display picture.
+     * @return Dialog box aligned for the user.
+     */
+    public static DialogBox getUserDialog(String message, Image image) {
+        return new DialogBox(message, image);
+    }
+
+    /**
+     * Creates a dialog box for a response sent by Duke.
+     *
+     * @param message Text sent by Duke.
+     * @param image Duke's display picture.
+     * @return Dialog box aligned for Duke.
+     */
+    public static DialogBox getDukeDialog(String message, Image image) {
+        DialogBox dialogBox = new DialogBox(message, image);
+        dialogBox.flip();
+        return dialogBox;
     }
 }
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,24 @@
+/**
+ * Generates Duke's responses to user messages.
+ */
 public class Duke {
+
+    /**
+     * Prints Duke's original command-line greeting.
+     *
+     * @param args Command-line arguments supplied to the program.
+     */
     public static void main(String[] args) {
         System.out.println("Hello!");
+    }
+
+    /**
+     * Generates a response for the user's chat message.
+     *
+     * @param input User's chat message.
+     * @return Duke's response to the message.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
     }
 }
